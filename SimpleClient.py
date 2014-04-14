236750ef5ead5ef74382a0996f578fef7398e726
Harshita Karande <harshukarande@gmail.com>
2014-04-14 19:59:40 -0400

    Changed try except logic for SimpleServer and updated SimpleClient

--- SimpleClient.py
+++ SimpleClient.py
@@ -1,14 +1,43 @@
 #!/usr/bin/python
 
+import socket
 import sys
-import remote
+import os
+import random
 
-if __name__ == "__main__":
-
-	if len(sys.argv)<2:
-		print 'Please provide the name of the file to read'
-	else:
-		try:
-			remote.copy('arjuna',12345,sys.argv[1])	
-		except Exception, e:
-			print e 
+s = socket.socket()
+host = socket.gethostname()
+port = 12345
+if len(sys.argv)<2:
+	print 'Please provide the name of the file to read'
+else:
+	try:
+		s.connect((host,port))
+		filename=sys.argv[1]
+		print filename
+		s.send(filename)
+		fullmessage=""
+	
+		returncode=s.recv(1)
+		while True:
+			data=s.recv(5)
+			if not data: break
+			fullmessage=fullmessage+data
+		s.close
+		if ord(returncode)==0:
+			tempname=filename
+			extindex=filename.index(".")
+			if extindex!=0:
+				ext=filename[extindex:]
+			while os.path.isfile(tempname):
+				randnum=random.randint(1,10)
+				file=tempname[:extindex]
+				tempname=file+str(randnum)+ext
+			print 'writing to new file '+tempname
+			f = open(tempname,"w")
+			f.write(fullmessage)
+			f.close()
+		else:
+			print fullmessage	
+	except:
+		print 'Failed to connect to host:',host,' port:',port
--- SimpleServer.py
+++ SimpleServer.py
@@ -2,7 +2,12 @@
 
 import socket
 
-
+class UserError(Exception):
+	def __init__(self, message):
+		self.message = message
+	def __str__(self):
+		return repr(self.message)
+		
 if __name__ == "__main__":
 	s = socket.socket()
 	#host = socket.gethostname()
@@ -18,16 +23,20 @@ if __name__ == "__main__":
 		print 'Got connection from', addr
 		extindex=filename.index(".")
 		ext=set([filename[extindex:]])
-		if filename!="":
-			if len(ext.intersection(whitelist))==1:
-				try:
-					with open(filename, "r") as myfile:
-						data=myfile.read()
-						c.send('\x00'+data)
-				except:
-					c.send('\x01Unable to open a file')
-			else:
-				c.send('\x01Sorry!This format is not in whitelist')
-		else:
-			c.send('\x01No file name received')
+		try:
+			if len(filename) == 0:
+				raise UserError('No filename recived')
+
+			if len(ext.intersection(whitelist)) != 1:
+				raise UserError('Sorry! This format is not in whitelist')
+
+			with open(filename, "r") as myfile:
+				data = myfile.read()
+				c.send('\x00'+data)
+
+		except UserError as e:	
+			c.send('\x01' + e.message)	
+		except Exception, e:
+			c.send('\x02' + str(e))
+			
 		c.close()
