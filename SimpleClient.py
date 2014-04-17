824a1358851285be16720288b4e0902046510fee
Harshita Karande <harshukarande@gmail.com>
2014-04-16 21:04:08 -0400

    Added switches to command line arguments, fixed issue with hostname

--- SimpleClient.py
+++ SimpleClient.py
@@ -4,40 +4,59 @@ import socket
 import sys
 import os
 import random
+import getopt
+
 
 s = socket.socket()
-host = socket.gethostname()
-port = 12345
-if len(sys.argv)<2:
-	print 'Please provide the name of the file to read'
-else:
-	try:
-		s.connect((host,port))
-		filename=sys.argv[1]
-		print filename
-		s.send(filename)
-		fullmessage=""
+host = ''
+port = ''
+filename = ''
+
+usage = 'SimpleClient.py -f <filename> -s <hostname> -p <portname>'
+try:
+	opts, args = getopt.getopt(sys.argv[1:], "hf:s:p:")
+except getopt.GetoptError:
+	print usage
+	sys.exit()
+for opt, arg in opts:
+	if opt == '-h':
+		print usage
+		sys.exit()
+	elif opt == '-f':
+		filename = arg 
+	elif opt == '-s':
+		host = arg
+	elif opt == '-p':
+		port = int(arg)
+if filename == '' or host == '' or port == '':
+	print usage
+	sys.exit()
+
+try:
+	s.connect((host,port))
+	s.send(filename)
+	fullmessage=""
 	
-		returncode=s.recv(1)
-		while True:
-			data=s.recv(5)
-			if not data: break
-			fullmessage=fullmessage+data
-		s.close
-		if ord(returncode)==0:
-			tempname=filename
-			extindex=filename.index(".")
-			if extindex!=0:
-				ext=filename[extindex:]
-			while os.path.isfile(tempname):
-				randnum=random.randint(1,10)
-				file=tempname[:extindex]
-				tempname=file+str(randnum)+ext
-			print 'writing to new file '+tempname
-			f = open(tempname,"w")
-			f.write(fullmessage)
-			f.close()
-		else:
-			print fullmessage	
-	except:
-		print 'Failed to connect to host:',host,' port:',port
+	returncode=s.recv(1)
+	while True:
+		data=s.recv(5)
+		if not data: break
+		fullmessage=fullmessage+data
+	s.close
+	if ord(returncode)==0:
+		tempname=filename
+		extindex=filename.index(".")
+		if extindex!=0:
+			ext=filename[extindex:]
+		while os.path.isfile(tempname):
+			randnum=random.randint(1,10)
+			file=tempname[:extindex]
+			tempname=file+str(randnum)+ext
+		print 'writing to new file '+tempname
+		f = open(tempname,"w")
+		f.write(fullmessage)
+		f.close()
+	else:
+		print fullmessage	
+except:
+	print 'Failed to connect to host:',host,' port:',port
